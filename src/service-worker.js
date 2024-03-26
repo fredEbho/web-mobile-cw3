e33e81d76d6f5c97c9a776b91fb9576fe6b4d68a
fredebho <fredebho2017@gmail.com>
2024-03-26 12:26:45 +0400

    debugging

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,13 +1,21 @@
 /* eslint-disable */
+const { precacheAndRoute, cleanupOutdatedCaches } = workbox.precaching;
+const { registerRoute } = workbox.routing;
+const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
+const { Plugin: ExpirationPlugin } = workbox.expiration;
+
 const { Plugin: CacheableResponsePlugin } = workbox.cacheableResponse;
-import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
-import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
-import {  ExpirationPlugin } from 'workbox-expiration';
+// import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
+// import { registerRoute } from 'workbox-routing';
+// import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+// import {  ExpirationPlugin } from 'workbox-expiration';
+
+console.log(precacheAndRoute)
 
 cleanupOutdatedCaches()
 
 precacheAndRoute(self.__WB_MANIFEST)
+
 
 self.addEventListener("message", (event) => {
     if (event.data && event.data.type === "SKIP_WAITING") {
@@ -15,6 +23,12 @@ self.addEventListener("message", (event) => {
     }
 });
 
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+precacheAndRoute(self.__precacheManifest, {});
 
 // cache image and render from the cache if it exists or go t the network
 registerRoute(
--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,6 @@ module.exports = defineConfig({
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       swSrc: "./src/service-worker.js",
+      exclude: ['_redirects']
     }}
 })
